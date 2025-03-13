559896074ac88b536183ca01298d5b158b48f6b3
KhaiNguyen <khaitom1911@Khai.nq213964sis.hust.edu.vn>
2025-03-13 08:57:01 +0700

    refactor: database module

---
+++ src/database/database.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from './entities/user.entity';
+import { RoleEntity } from './entities/role.entity';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DATABASE_HOST'),
+        port: configService.get<number>('DATABASE_PORT'),
+        username: configService.get<string>('DATABASE_USER'),
+        password: configService.get<string>('DATABASE_PASSWORD'),
+        database: configService.get<string>('DATABASE_NAME'),
+        entities: [UserEntity, RoleEntity],
+        synchronize: true,
+      }),
+    }),
+  ],
+})
+export class DatabaseModule {}
