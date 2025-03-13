4a4d4bffbeaddc898eaa7b060fc93ed33a347bdf
Khai Nguyen <104447228+Khainq1911@users.noreply.github.com>
2025-03-13 08:58:36 +0700

    refactor: database module
    * feat: auth
    
    * refactor: database module
    
    ---------
    
    Co-authored-by: KhaiNguyen <khaitom1911@Khai.nq213964sis.hust.edu.vn>

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,31 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule} from '@nestjs/config';
 import { AuthModule } from './modules/auth/auth.module';
-import { UserEntity } from './database/entities/user.entity';
-import { RoleEntity } from './database/entities/role.entity';
+import { DatabaseModule } from './database/database.module';
+
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get<string>('DATABASE_HOST'),
-        port: configService.get<number>('DATABASE_PORT'),
-        username: configService.get<string>('DATABASE_USER'),
-        password: configService.get<string>('DATABASE_PASSWORD'),
-        database: configService.get<string>('DATABASE_NAME'),
-        entities: [UserEntity, RoleEntity],
-        synchronize: true,
-      }),
-    }),
+
+    DatabaseModule,
     AuthModule,
   ],
   controllers: [AppController],
