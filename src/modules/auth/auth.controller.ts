31b73043155be14d03a02c45b0e6a8d725fde661
khainq1911 <khaitom1911.dev@gmail.com>
2025-04-22 00:46:32 +0700

    fix: authen and author

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,7 +1,9 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDto, RegisterDto } from './auth.dto';
 import { Public } from 'src/common/decorators/public.decorators';
+import { User } from 'src/common/decorators/user.decorators';
+import { UserEntity } from 'src/database/entities/user.entity';
 
 @Controller('auth')
 export class AuthController {
@@ -22,5 +24,11 @@ export class AuthController {
   @Post('refresh-token')
   refreshToken(@Body() { refreshToken }) {
     return this.authService.refreshToken(refreshToken);
+  }
+
+
+  @Get("test")
+  async findOne(@User() user: unknown) {
+    return user
   }
 }
--- src/modules/permit/permit.controller.ts
+++ src/modules/permit/permit.controller.ts
@@ -11,30 +11,27 @@ import {
 import { PermitService } from './permit.service';
 import { Public } from 'src/common/decorators/public.decorators';
 import { PermitDto, UpdatePermitDto } from './permit.dto';
+import { User, UserDTO } from 'src/common/decorators/user.decorators';
 
 @Controller('permit')
 export class PermitController {
   constructor(private readonly permitService: PermitService) {}
 
-  @Public()
   @Post()
   createPermit(@Body() payload: PermitDto) {
     return this.permitService.create(payload);
   }
 
-  @Public()
   @Patch(':id')
   updateStatus(@Body() payload: UpdatePermitDto, @Param('id') id: number) {
     return this.permitService.update(payload, id);
   }
 
-  @Public()
   @Get()
-  listPermit() {
-    return this.permitService.listPermit();
+  listPermit(@User() user: UserDTO) {
+    return this.permitService.listPermit(user);
   }
 
-  @Public()
   @UseInterceptors(ClassSerializerInterceptor)
   @Get(':id')
   getPermitById(@Param('id') id: number) {
